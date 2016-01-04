0fed8fabbe977f9c9697c6ca31a01a659769840b
Fear-Otaku <fear_otaku@hotmail.com>
2016-01-04 21:01:06 +1100

    Create help.php

---
+++ help.php
@@ -0,0 +1,18 @@
+<div id="settings_form" style="border: 1px solid #ccc; background: #eee;">
+	<div style="padding: 0 20px 20px;">
+		<div>
+			<fieldset>
+			<legend>
+			<h1><?php _e('Installation / FAQ', 'listcloud'); ?></h1>
+			</legend>
+			<h2><?php _e('What is List Tag Cloud?', 'listcloud'); ?></h2>
+			<p><?php _e('This Plugin allows you to show a Tag Cloud from specified keywords from the title of your classified listings and display it on any part of your site you want.', 'listcloud'); ?>
+			</p>
+			<h2><?php _e('How to install it?', 'listcloud'); ?></h2>
+			<p><?php _e('Insert the following two lines in either the main.php or any other page of your theme you want the tag cloud to appear.', 'listcloud'); ?>
+			</p>
+			<pre> &lt;?php listcloud_ShowCloud(); ?&gt; </pre>
+			<pre> &lt;?php listcloud_CloudDump() ?&gt;</pre>
+			</fieldset> </div>
+	</div>
+</div>
